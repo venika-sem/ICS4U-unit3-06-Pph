339a35a45893bac19def43c774e6fd03274a90e3
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2024-06-04 23:23:28 -0400

    done

--- main.php
+++ main.php
@@ -1,92 +1,58 @@
-/*This program is a merge sort program
- * *By Venika Sem
- * *@version 1.0
- * *@since Feb-2024
- * */
-
-// merge sort
-function mergeSort(arr: number[]): number[] {
-    if (arr.length < 2) {
-        return arr
+<?php
+    /**
+     * This program is a merge sort program
+     * By Venika Sem
+     * @version 1.0
+     * @since Feb-2024
+     */
+    
+    function merge($left, $right) {
+        $sortArray = [];
+    
+        while (count($left) && count($right)) {
+            if ($left[0] < $right[0]) {
+                array_push($sortArray, array_shift($left));
+            } else {
+                array_push($sortArray, array_shift($right));
+            }
+        }
+    
+        return array_merge($sortArray, $left, $right);
     }
-
-    // math.floor rounds down
-    const middle = Math.floor(arr.length / 2)
-    const left = arr.slice(0, middle)
-    const right = arr.slice(middle)
-
-    return merge(mergeSort(left), mergeSort(right))
-}
-
-// merge
-function merge(left: number[], right: number[]): number[] {
-    let result = []
-    let leftIndex = 0
-    let rightIndex = 0
-
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            result.push(left[leftIndex])
-            leftIndex++
+    
+    function mergeSort($sortArray) {    
+        $mid = count($sortArray) / 2;
+    
+        if (count($sortArray) < 2) {
+            return $sortArray;
+        }
+    
+        $left = array_splice($sortArray, 0, $mid);
+        return merge(mergeSort($left), mergeSort($sortArray));
+    }
+    
+    // Create array
+    $arrayLength = 5;
+    $counter = 0;
+    $array = [];
+    
+    while ($counter < $arrayLength) {
+        $selectedNumber = readline("Enter a number to put at [$counter]: ");
+        if (!is_numeric($selectedNumber)) {
+            echo "Invalid input.\n";
         } else {
-            result.push(right[rightIndex])
-            rightIndex++
+            // Round the floating-point number to the nearest integer
+            $array[] = round($selectedNumber);
+            $counter++;
         }
     }
-    // combine the two arrays into a sorted array
-    // concat adds the two arrays together
-    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
-}
-
-// create random arrays with no repeats (lenth 50, 100, 200)
-let arr50: number[] = []
-let arr100: number[] = []
-let arr200: number[] = []
-
-// array of 50 random numbers
-for (let counter = 0; counter < 50; counter++) {
-    let num = Math.floor(Math.random() * 50)
-    // make sure no numbers repeat
-    if (!arr50.includes(num)) {
-        arr50.push(num)
-    }
-}
-
-// array of 100 random numbers
-for (let counter = 0; counter < 100; counter++) {
-    let num = Math.floor(Math.random() * 100)
-    // make sure no numbers repeat
-    if (!arr100.includes(num)) {
-        arr100.push(num)
-    }
-}
-
-// array of 200 random numbers
-for (let counter = 0; counter < 200; counter++) {
-    let num = Math.floor(Math.random() * 200)
-    // make sure no numbers repeat
-    if (!arr200.includes(num)) {
-        arr200.push(num)
-    }
-}
-
-// main
-console.log("Original array of 50 random numbers: \n")
-console.log(arr50.join(", "))
-console.log("\nSorted array of 50 random numbers: \n")
-console.log(mergeSort(arr50).join(", "))
-
-console.log("\n**********\n")
-
-console.log("Original array of 100 random numbers: \n")
-console.log(arr100.join(", "))
-console.log("\nSorted array of 100 random numbers: \n")
-console.log(mergeSort(arr100).join(", "))
-
-console.log("\n**********\n")
-console.log("Original array of 200 random numbers: \n")
-console.log(arr200.join(", "))
-console.log("\nSorted array of 200 random numbers: \n")
-console.log(mergeSort(arr200).join(", "))
-
-console.log("\nDone.")
+    
+    // Show unsorted array
+    echo "Unsorted array: " . implode(', ', $array) . "\n";
+    
+    // Sort array
+    $sortedArray = mergeSort($array);
+    echo "Sorted array: " . implode(', ', $sortedArray) . "\n";
+    
+    echo "\nDone.\n";
+?>
